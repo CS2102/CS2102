8d24b67a9b7fd555b09b81c279a1a108e10b1299
Brett Ammeson <ammesonb@gmail.com>
2012-11-01 21:11:49 -0400

    Priority Queue

--- Homework2/src/PriorityQueue.java
+++ Homework2/src/PriorityQueue.java
@@ -1,4 +1,12 @@
 
 public interface PriorityQueue {
-
+	
+	PriorityQueue newPQ(); // produces a priority queue with no elements
+	
+	PriorityQueue addElt(PriorityQueue q, int elem); // adds element
+	
+	PriorityQueue remMinElt(PriorityQueue q); // remove smallest element
+	
+	int getMinElt(PriorityQueue q);  // return, but don’t remove, smallest elt
+	
 }
