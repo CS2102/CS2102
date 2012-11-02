37c16a3735ea501385b5378a36c500045484d2d8
Josh <Wasteland7@gmail.com>
2012-11-01 21:18:14 -0400

    Updated ADT interfaces

--- Homework2/src/Queue.java
+++ Homework2/src/Queue.java
@@ -1,10 +1,19 @@
 
 public interface Queue {
 	
-	Queue newQ();
+	Queue newQ();  // produces a Queue with no elements
 	
-	Queue enqueue(int elem);
+	Queue enqueue(int elem);  // adds element
+	
+	Queue dequeue();  // remove least recently-added element
+	
+	int front();  // return, but don't remove, least-recently added element
 	
 	
+	/*
+	 * Example: Assume that we added elements 7, 4, and 5 to a new queue (in that order). 
+	 * Calling dequeue would produce a queue containing 4 and 5. Calling front on that stack 
+	 * would produce 4. Calling dequeue on that stack again would produce a stack containing 5.
+	 */
 	
 }
