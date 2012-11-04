dcadf88d13b2343569abb878cd58d48108d82c18
Josh <Wasteland7@gmail.com>
2012-11-04 17:45:03 -0500

    Fixed AVLTree

--- Homework2/src/AVLTree.java
+++ Homework2/src/AVLTree.java
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 public interface AVLTree {
 
 	int size();
@@ -13,46 +12,4 @@ public interface AVLTree {
 	boolean hasElem(int elem);
 
 	boolean isBalanced();
-=======
-public class AVLTree implements ISet {
-	int size;
-	
-	AVLTree() {
-		this.size = 0;
-	}
-
-	public ISet addElem(int elem) {
-		rebalance();
-		return this;
-	}
-
-	public ISet removeElem(int elem) {
-		rebalance();
-		return this;
-	}
-
-	public boolean hasElem(int elem) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	private AVLTree rebalance() {
-		if (isBalanced()) {
-			return this;
-		}
-		else {
-			// TODO: Balance code
-			return this;
-		}
-	}
-	
-	private boolean isBalanced() {
-		return true;
-	}
->>>>>>> 26de3cc81cd069ba3feb6af3727dca43dc16f99a
 }
