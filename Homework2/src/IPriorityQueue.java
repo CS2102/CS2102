ffe02df3bccd288b47a964a61f437c786b5f43d7
Brett Ammeson <ammesonb@gmail.com>
2012-11-06 20:30:44 -0500

    Renamed Stupid Interfaces

---
+++ Homework2/src/IPriorityQueue.java
@@ -0,0 +1,16 @@
+
+public interface IPriorityQueue {
+	
+	IPriorityQueue addElt(int elem); // adds element
+	
+	IPriorityQueue remMinElt(); // remove smallest element
+	
+	int getMinElt();  // return, but don’t remove, smallest elt
+	
+	/*
+	 * Example: Assume we added elements 7, 4, and 5 to a new priority queue (in that order). 
+	 * Calling getMinElt on the resulting priority queue should return 4. Calling remMinElt 
+	 * should produce a priority queue containing only 5 and 7.
+	 */
+	
+}
