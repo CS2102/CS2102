b05cc24985564cfc3306b231941b1165c8403659
Brett Ammeson <ammesonb@gmail.com>
2012-11-08 21:03:44 -0500

    Added Classes and Accessors

---
+++ Homework3/src/Date.java
@@ -0,0 +1,38 @@
+
+public class Date {
+	int year;
+	int month;
+	int day;
+
+	/**
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public Date(int year, int month, int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/**
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
+}
---
+++ Homework3/src/Reading.java
@@ -0,0 +1,38 @@
+
+public class Reading {
+	double temp;
+	int hour;
+	int minute;
+	
+	/**
+	 * @param temp
+	 * @param hour
+	 * @param minute
+	 */
+	public Reading(double temp, int hour, int minute) {
+		this.temp = temp;
+		this.hour = hour;
+		this.minute = minute;
+	}
+
+	/**
+	 * @return the temp
+	 */
+	public double getTemp() {
+		return temp;
+	}
+
+	/**
+	 * @return the hour
+	 */
+	public int getHour() {
+		return hour;
+	}
+
+	/**
+	 * @return the minute
+	 */
+	public int getMinute() {
+		return minute;
+	}
+}
