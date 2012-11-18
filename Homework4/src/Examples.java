73cb9dc2a7e590dcec4d2d2ab9d9f4fd1cbe4079
Josh <Wasteland7@gmail.com>
2012-11-18 18:00:53 -0500

    Some test methods

--- Homework4/src/Examples.java
+++ Homework4/src/Examples.java
@@ -3,10 +3,21 @@ import tester.Tester;
 
 public class Examples {
 	Examples() {}
-	DataHeap<Integer> h = new DataHeap<Integer>(5, 
-			new MtHeap<Integer>(), new DataHeap<Integer>(2));
+	
 	
 	boolean test1(Tester t) {
-		return t.checkExpect(true, true);
+		MtHeap<Integer> h = new MtHeap<Integer>();
+		return t.checkExpect(h.isAddValid(5), true);
+	}
+	
+	boolean test2(Tester t) {
+		DataHeap<Integer> h = new DataHeap<Integer>(5, 
+				new MtHeap<Integer>(), new DataHeap<Integer>(2));
+		return t.checkExpect(h.isHeap(), false);
+	}
+	
+	boolean test3(Tester t) {
+		DataHeap<Integer> h = new DataHeap<Integer>(3, new MtHeap<Integer>(), new MtHeap<Integer>());
+		return t.checkExpect(h.isRemMinValid(), true);
 	}
 }
