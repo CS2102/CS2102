564c70ec0876b4660e963264a2a781a68cec3dd9
Josh <Wasteland7@gmail.com>
2012-11-18 17:41:58 -0500

    Add/RemoveValid methods

--- Homework4/src/Heap.java
+++ Homework4/src/Heap.java
@@ -4,25 +4,25 @@ import java.util.Random;
 interface IHeap<T extends Comparable<T>> extends IBinTree<T> {
 	// adds given element to the heap without removing other elements
 	IHeap<T> addElt(T e);
-	
+
 	// removes one occurrence of the smallest element from the heap
 	IHeap<T> remMinElt();
-	
+
 	// Merge the current heap with another heap
 	IHeap<T> merge(IHeap<T> withHeap);
-	
+
 	// Determine if the root of this heap is bigger than the given element
 	boolean isBigger(T e);
-	
+
 	// for Tests
 	boolean isHeap();
-	
+
 	boolean isAddValid(T elem);
-	
+
 	boolean isRemMinValid();
-	
+
 	boolean isEmpty();
-	
+
 	LinkedList<T> toList();
 }
 
@@ -70,30 +70,30 @@ class MtHeap<T extends Comparable<T>> implements IHeap<T> {
 	public IHeap<T> merge(IHeap<T> withHeap) {
 		return withHeap;
 	}
-	
+
 	// Testers
 	public boolean isHeap(){
 		return true;
 	}
-	
+
 	public boolean isEmpty(){
 		return true;
 	}
-	
+
 	public boolean isAddValid(T elem){
 		IHeap<T> h = this.addElt(elem);
-		
+
 		if(h.isEmpty())
 			return false;
 		DataHeap<T> d = (DataHeap<T>) h;
 		return d.data == elem && d.left.isEmpty() && d.right.isEmpty();
 	}
-	
+
 	public boolean isRemMinValid() {
 		IHeap<T> h = this.remMinElt();
 		return h.isEmpty();
 	}
-	
+
 	public LinkedList<T> toList(){
 		return new LinkedList<T>();
 	}
@@ -108,7 +108,7 @@ class DataHeap<T extends Comparable<T>> extends DataBT<T> implements IHeap<T> {
 		this.left = new MtHeap<T>();
 		this.right = new MtHeap<T>();
 	}
-	
+
 	DataHeap(T data, IHeap<T> left, IHeap<T> right) {
 		super(data, left, right);
 		this.left = left;
@@ -182,32 +182,58 @@ class DataHeap<T extends Comparable<T>> extends DataBT<T> implements IHeap<T> {
 	public boolean isBigger(T e) {
 		return (this.data.compareTo(e))>0;
 	}
-	
+
 	// Testers
 	public boolean isEmpty() {
 		return false;
 	}
-	
+
 	public boolean isHeap(){
 		return this.left.isBigger(this.data) &&
 				this.right.isBigger(this.data) &&
 				this.left.isHeap() &&
 				this.right.isHeap();
 	}
-	
+
 	public boolean isAddValid(T e) {
 		IHeap<T> h = this.addElt(e);
 		LinkedList<T> origElems = this.toList();
 		origElems.add(e);
 		LinkedList<T> newElems = h.toList();
-		// make sure that the original and new lists are identical
-		
+		// make sure that the original and new lists are identical,
+		// not necessarily same order
+		for(int i = 0; i < origElems.size(); i++){
+			if(!newElems.contains(origElems.get(i)))
+				return false;
+			newElems.remove(origElems.get(i));
+		}
+		return newElems.isEmpty() &&
+				h.isHeap();
 	}
-	
+
 	public boolean isRemMinValid(){
-		
+		IHeap<T> h = this.remMinElt();
+		LinkedList<T> origElems = this.toList();
+		T min = origElems.getFirst();
+		for(int i =1; i < origElems.size(); i++){
+			if(min.compareTo(origElems.get(i)) > 0)
+				min = origElems.get(i);
+		}
+		// now min is the minimum valued element in the original list
+		origElems.remove(min);
+
+		LinkedList<T> newElems = h.toList();
+		// make sure that the original and new lists are identical,
+		// not necessarily same order
+		for(int i = 0; i < origElems.size(); i++){
+			if(!newElems.contains(origElems.get(i)))
+				return false;
+			newElems.remove(origElems.get(i));
+		}
+		return newElems.isEmpty() &&
+				h.isHeap();
 	}
-	
+
 	public LinkedList<T> toList(){
 		LinkedList<T> leftList = this.left.toList(), 
 				rightList = this.right.toList();
