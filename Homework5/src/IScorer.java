aa7c522c15de7491de4f39a856eef84c9f6d1841
Brett Ammeson <ammesonb@gmail.com>
2012-12-02 17:54:46 -0500

    Tentative Final

---
+++ Homework5/src/IScorer.java
@@ -0,0 +1,4 @@
+
+public interface IScorer {
+	int computeScore(String name, IVoteData vd);
+}
---
+++ Homework5/src/Score1.java
@@ -0,0 +1,10 @@
+//Scoring Implementations
+public class Score1 implements IScorer {
+
+	@Override
+	public int computeScore(String name, IVoteData vd) {
+		return vd.getFirstChoiceCount(name);
+	}
+	
+}
+
